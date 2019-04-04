63451a34da284e4079b2ef381ba484690537262e
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-04 15:04:12 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -17,6 +17,8 @@ class Timer extends Component {
   componentDidUpdate() {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
+  
+  
 
 
   componentDidMount() {
